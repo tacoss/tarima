05d5b46b20a900dd204bc80527450269dfd7711a
Alvaro Cabrera <pateketrueke@gmail.com>
2014-01-28 16:03:08 -0600

    Fixed us-engine

--- src/engines/us.js
+++ src/engines/us.js
@@ -2,17 +2,20 @@
 register_engine('us', function(params) {
   var tpl = _.template(params.source, null, defs_tpl('lodash', params.options));
 
-
-  if ('js' === params.next) {
-    return tpl.toString();
-  }
-
-  if ('html' === params.next) {
-    return tpl(params.options.locals);
+  switch (params.next) {
+    case 'js':
+      return tpl.toString();
+    case 'md':
+    case 'hbs':
+    case 'jade':
+    case 'html':
+    case 'less':
+    case 'coffee':
+      return tpl(params.options.locals);
   }
 
   if (!params.next) {
-    return params.call ? tpl.toString(): tpl;
+    return tpl;
   }
 
   return params.source;
