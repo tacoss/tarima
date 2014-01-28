ee1e385192f90ccfca81f307d0fee2ee8a43f3e2
Alvaro Cabrera <pateketrueke@gmail.com>
2014-01-28 01:52:15 -0600

    Adds support for _.template

--- src/engines/us.js
+++ src/engines/us.js
@@ -1,4 +1,19 @@
 
 register_engine('us', function(params) {
-  return _.template(params.source, null, defs_tpl('lodash', params.options));
+  var tpl = _.template(params.source, null, defs_tpl('lodash', params.options));
+
+
+  if ('js' === params.next) {
+    return tpl.toString();
+  }
+
+  if ('html' === params.next) {
+    return tpl(params.options.locals);
+  }
+
+  if (!params.next) {
+    return params.call ? tpl.toString(): tpl;
+  }
+
+  return params.source;
 });
