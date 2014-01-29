49917bab61f2f3ced694a934bdd8e63568ed7825
Alvaro Cabrera <pateketrueke@gmail.com>
2014-01-29 07:17:53 -0600

    Clean code for .coffee.litcoffee transforms; fix

--- src/engines/coffee.js
+++ src/engines/coffee.js
@@ -1,6 +1,28 @@
 
+var cleanLiterate = function(code) {
+  var maybe_code = true, match,
+      out = [], lines = code.split('\n');
+
+  for (var line in lines) {
+    line = lines[line];
+    match = line.match(/^([ ]{4}|[ ]{0,3}\t)/);
+
+    if (maybe_code && match) {
+      out.push(line.substr(match[1].length));
+    } else {
+      maybe_code = /^\s*$/.test(line);
+    }
+  }
+
+  return out.join('\n');
+};
+
 register_engine('coffee', function(params, coffee) {
   var body, tpl, fn;
+
+  if (params.options.literate && 'coffee' === params.next) {
+    params.source = cleanLiterate(params.source);
+  }
 
   tpl =  coffee.compile(params.source, defs_tpl('coffee', params.options));
   body = 'with(locals_||{}){' + tpl.toString() + '};return "";';
