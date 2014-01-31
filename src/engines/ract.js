eba4bce1182249754db5f614cc0033631f42c52a
Alvaro Cabrera <pateketrueke@gmail.com>
2014-01-31 13:33:03 -0600

    Fixing ract-engine

--- src/engines/ract.js
+++ src/engines/ract.js
@@ -1,17 +1,17 @@
 
-register_engine('ract', function(params) {
-  var tpl = require('ractive').parse(params.source, defs_tpl('ractive', params.options)),
+register_engine('ract', function(params, next) {
+  var Ractive = require('ractive');
+
+  var tpl = Ractive.parse(params.source, defs_tpl('ractive', params.options)),
       body = 'return ' + ('string' === typeof tpl ? tpl : JSON.stringify(tpl)) + ';',
       fn;
 
   /* jshint evil:true */
   fn = new Function('', body);
 
-  if ('js' === params.next) {
+  if (next('js')) {
     return fn.toString();
   }
 
-  if ('ract' !== params.next) {
-    return !params.call ? params.source : fn;
-  }
+  return fn;
 });
