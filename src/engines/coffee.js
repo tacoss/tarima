2a9f1af2363854e82a4fa80047a44ff160e424d4
Alvaro Cabrera <pateketrueke@gmail.com>
2014-01-31 13:32:07 -0600

    Fixing coffee-engine

--- src/engines/coffee.js
+++ src/engines/coffee.js
@@ -1,5 +1,5 @@
 
-var cleanLiterate = function(code) {
+var cleanLiterate = function(code, strip) {
   var maybe_code = true, match,
       out = [], lines = code.split('\n');
 
@@ -11,30 +11,33 @@ var cleanLiterate = function(code) {
       out.push(line.substr(match[1].length));
     } else {
       maybe_code = /^\s*$/.test(line);
+
+      if (maybe_code && !strip) {
+        out.push('# ' + line);
+      }
     }
   }
 
   return out.join('\n');
 };
 
-register_engine('coffee', function(params, coffee) {
-  var body, tpl, fn;
+register_engine('coffee', function(params, next) {
+  var coffee = require('coffee-script');
 
-  if (params.options.literate && 'coffee' === params.next) {
-    params.source = cleanLiterate(params.source);
+  if (params.options.literate && next('coffee')) {
+    params.source = cleanLiterate(params.source, params.options.strip);
   }
 
-  tpl =  coffee.compile(params.source, defs_tpl('coffee', params.options));
-  body = 'with(locals_||{}){' + tpl.toString() + '};return "";';
+  var tpl =  coffee.compile(params.source, defs_tpl('coffee', params.options)),
+      body = 'with(locals_||{}){' + tpl.toString() + '}',
+      fn;
 
   /* jshint evil:true */
   fn = new Function('locals_', body);
 
-  if ('js' === params.next) {
+  if (next('js')) {
     return fn.toString();
   }
 
-  if ('coffee' !== params.next) {
-    return !params.call ? fn(params.options.locals) : fn;
-  }
-}, require('coffee-script'));
+  return fn;
+});
