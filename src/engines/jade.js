59d67ba52d29a97aeacf858e9d5b770590b5fd8f
Alvaro Cabrera <pateketrueke@gmail.com>
2014-03-03 10:27:32 -0600

    Avoid render() method from jade-engine; fix

--- src/engines/jade.js
+++ src/engines/jade.js
@@ -2,18 +2,16 @@
 register_engine('jade', function(params, next) {
   var jade = require('jade');
 
-  var compile = function(client, render) {
+  var compile = function(client) {
     params.options.client = client;
 
-    return jade[render ? 'render' : 'compile'](params.source, defs_tpl('jade', params.options));
+    return jade.compile(params.source, defs_tpl('jade', params.options));
   };
 
   if (!params.call || next('js', 'html', 'ract')) {
     if (!params.next && 'js' === params.ext) {
       return compile(true).toString();
     }
-
-    return compile(false, true);
   }
 
   return compile();
