7bc371ed0563673587cc99eac645e2d13274e56f
Alvaro Cabrera <pateketrueke@gmail.com>
2014-08-05 15:40:24 -0500

    Workaround for ractive-5.x precompiled templates; fix

--- src/engines/ract.js
+++ src/engines/ract.js
@@ -5,7 +5,7 @@ register_engine('ract', function(params, next) {
   switch (next('js', 'us', 'hbs', 'html')) {
     case 'js':
       var tpl = Ractive.parse(params.source, defs_tpl('ractive', params)),
-          body = 'return ' + JSON.stringify(tpl) + ';';
+          body = 'var o=' + JSON.stringify(tpl) + ';return o.v&&o.t||o;';
 
       if (!params.chain) {
         return 'function(){' + body + '}';
